03b6baca7f02b69bc1e468989811551428722d45
Sara Souza <sarathalitajh@gmail.com>
2026-01-30 10:02:19 -0400

    Adicionando interfaces

--- ScreenMatch/2887-java-screenmatch-oo/src/Principal.java
+++ ScreenMatch/2887-java-screenmatch-oo/src/Principal.java
@@ -1,4 +1,6 @@
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+import br.com.alura.screenmatch.calculos.FiltroRecomendacao;
+import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
@@ -42,6 +44,14 @@ public class Principal {
         calculadora.inclui(lost);
         System.out.println(calculadora.getTempoTotal());
 
+        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        filtro.filtra(meuFilme);
+
+        Episodio episodio= new Episodio();
+        episodio.setNumero(1);
+        episodio.setSerie(String.valueOf(lost));
+        episodio.setTotalVisualizacoes(300);
+        filtro.filtra(episodio);
     }
 
 }
